a7d6a3d47f4aca37ad0268c8bf6ad41005bfe130
tiago lage <79291364+teizon@users.noreply.github.com>
2023-02-21 16:24:50 -0300

    Add files via upload

---
+++ teste_iot1.ino
@@ -0,0 +1,96 @@
+//I2S driver
+#include <driver/i2s.h>
+ 
+// Connections to INMP441 I2S microphone
+#define I2S_WS 25
+#define I2S_SD 33
+#define I2S_SCK 32
+ 
+// Use I2S Processor 0
+#define I2S_PORT I2S_NUM_0
+ 
+// Define input buffer length
+#define bufferLen 64
+int16_t sBuffer[bufferLen];
+
+#define SAMPLE_BITS 16
+ 
+void i2s_install() {
+  // Set up I2S Processor configuration
+  const i2s_config_t i2s_config = {
+      .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+      .sample_rate = 44100,
+      .bits_per_sample = i2s_bits_per_sample_t(SAMPLE_BITS),
+      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+      .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // default interrupt priority
+      .dma_buf_count = 4,
+      .dma_buf_len = 1024,
+      .use_apll = true
+      };
+
+ 
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+}
+ 
+void i2s_setpin() {
+  // Set I2S pin configuration
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = -1,
+    .data_in_num = I2S_SD
+  };
+ 
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
+ 
+void setup() {
+ 
+  // Set up Serial Monitor
+  Serial.begin(9600);
+  Serial.println(" ");
+ 
+  delay(1000);
+ 
+  // Set up I2S
+  i2s_install();
+  i2s_setpin();
+  i2s_start(I2S_PORT);
+ 
+ 
+  delay(500);
+}
+ 
+void loop() {
+ 
+  // False print statements to "lock range" on serial plotter display
+  // Change rangelimit value to adjust "sensitivity"
+  int rangelimit = 3000;
+  Serial.print(rangelimit * -1);
+  Serial.print(" ");
+  Serial.print(rangelimit);
+  Serial.print(" ");
+ 
+  // Get I2S data and place in data buffer
+  size_t bytesIn = 0;
+  esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+ 
+  if (result == ESP_OK)
+  {
+    // Read I2S data buffer
+    int16_t samples_read = bytesIn / 8;
+    if (samples_read > 0) {
+      float mean = 0;
+      for (int16_t i = 0; i < samples_read; ++i) {
+        mean += (sBuffer[i]);
+      }
+ 
+      // Average the data reading
+      mean /= samples_read;
+ 
+      // Print to serial plotter
+      Serial.println(mean);
+    }
+  }
+}
